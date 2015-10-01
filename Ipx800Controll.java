9f290778344d68f3ec7f16789c6e577de5288b74
Bruno Boettcher <bboett@gmail.com>
2015-10-01 22:23:03 +0200

    Ok, added all the other methods

--- Ipx800Controll.java
+++ Ipx800Controll.java
@@ -53,7 +53,8 @@ public class Ipx800Control
 	public boolean set(int relai, boolean etat, boolean impulsion)
 	{
 		String cmd = "Set";
-		if (relai < 0 || relai > 32) return false;
+		if (relai < 1 || relai > 32) return false;
+		if(relai < 10) cmd += "0"; 
 		cmd += relai;
 		if (etat) cmd += "1";
 		else cmd += "0";
@@ -87,7 +88,7 @@ public class Ipx800Control
 	 	 */
 	public boolean getIn(int relai)
 	{
-		if (relai <0 ||relai > 32) return false;
+		if (relai <1 ||relai > 32) return false;
 
 		String cmd = "GetIn"+relai;
 		
@@ -146,31 +147,154 @@ public class Ipx800Control
 	   GetAnx ou x est le numéro de l’entrée analogique de 1 à 4 
 	   GetAn1 permet de recevoir l’état de l’entrée analogique 1. 
 	   Réponse de l’IPX800 GetAn1=512  (valeur de 0 à 1023)           
-	   
+	 */
+	public int getAn(int relai)
+	{
+		if (relai <1 ||relai > 4) return -1;
+
+		String cmd = "GetAn"+relai;
+
+		int retVal = -1;
+
+		if(sendCmd(cmd))
+		{
+			String[] parts = returnMsg.split("=");
+			//String part1 = parts[0]; // Getin
+			String part2 = parts[1]; // value
+			try
+			{
+				retVal = Integer.parse(part2);
+			}
+			catch(NumberFormatException e)
+			{
+				System.err.println("no number: "+part2);
+			}
+		}
+
+		return retVal;
+	}
+	/*
+	 
 	   ••••                        Obtenir l‘état d’un compteur d’implusion :  
 	   GetCount Paramètres : GetCountx ou x est le numéro de compteur (de 1 à 3) 
 	   GetCount1 renvoi la valeur du compteur 1. 
 	   Réponse de l’IPX GetCount1=0 (Compteur 32 Bits soit une valeur de 0 à 4294967295)
-	   
-	   ••••                        Obtenir l‘état d’une sortie:  
+	 */
+	public int getCount(int relai)
+	{
+		if (relai <1 ||relai > 3) return -1;
+
+		String cmd = "GetCount"+relai;
+
+		int retVal = -1;
+
+		if(sendCmd(cmd))
+		{
+			String[] parts = returnMsg.split("=");
+			//String part1 = parts[0]; // Getin
+			String part2 = parts[1]; // value
+			try
+			{
+				retVal = Integer.parse(part2);
+			}
+			catch(NumberFormatException e)
+			{
+				System.err.println("no number: "+part2);
+			}
+		}
+		return retVal;
+	}
+	/*
+	 
+	    Obtenir l‘état d’une sortie:  
 	   GetOut Paramètres : GetOutx ou x est le numéro de la sortie (de 1 à 32). 
 	   GetOut1 renvoi la valeur de la sorite 1. 
 	   Réponse de l’IPX800 GetOut1=1 (Valeur 0 ou 1 en fonction de l’état de la sortie). 
-	   
+	 */
+	public boolean getOut(int relai)
+	{
+		if (relai <1 ||relai > 32) return false;
+
+		String cmd = "GetOut"+relai;
+
+		boolean retVal = false;
+
+		if(sendCmd(cmd))
+		{
+			String[] parts = returnMsg.split("=");
+			//String part1 = parts[0]; // Getin
+			String part2 = parts[1]; // value
+			if(part2.equals("1")) retVal = true;
+		}
+
+		return retVal;
+	}
+	/*
+	 
 	   ••••                        Obtenir l‘état de toute les  sorties:  
 	   GetOutputs Paramètres : 
 	   GetOutputs   permet de recevoir dans une seule trame l’état des 32 sorties. 
 	   Réponse de l’IPX800: GetOutputs=00000000000000000000000000000000  
 	   (Le dernier caractère reçu correspond à la sortie 32)            
-	   
+	 */
+	public boolean[] getOut()
+	{
+		String cmd = "GetOutputs";
+
+		boolean[] retVal = new boolean[32];
+
+		if(sendCmd(cmd))
+		{
+			String[] parts = returnMsg.split("=");
+			//String part1 = parts[0]; // Getin
+			String part2 = parts[1]; // value
+			for(int i =0; i<32;i++)
+			{
+				if(part2.charAt(i) == '1') retVal[i] = true;
+				else retVal[i] = false;
+			}
+
+		}
+		else
+		{
+			for(int i =0; i<32;i++)
+			{
+				retVal[i] = false;
+			}
+		}
+
+		return retVal;
+	}
+	/*
+	 
 	   ••••                        Remise à zéro des compteurs:  
 	   ResetCount Paramètres : 
 	   ResetCountx ou x est le numéro du compteur à remettre à zéro. 
 	   ResetCount1 remet le compteur 1 à zéro. 
 	   Réponse de l’IPX800: Success •• • •                  
-	   
+	 */
+	public boolean resetCount(int relai)
+	{
+		if (relai <1 ||relai > 3) return false;
+
+		String cmd = "ResetCount"+relai;
+
+		boolean retVal = false;
+
+		if(sendCmd(cmd) && returnMsg.equals("Success"))
+			 retVal = true;
+
+		return retVal;
+	}
+	/*
+	 
 	   Reset système de l’IPX800  Reset Paramètres  : 
 	   Reset L’envoi de cette commande provoque le redémarrage de l’IPX800 (Reboot). 
 	   
 	*/
+	public void reset()
+	{
+		String cmd = "Reset";
+		sendCmd(cmd);
+	}
 }
